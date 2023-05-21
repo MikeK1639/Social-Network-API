6bba79b0792372e739dfa0aaba5459fb8df444a8
MikeK1639 <126822125+MikeK1639@users.noreply.github.com>
2023-05-21 19:15:23 -0400

    restructuring models and routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,57 +1,78 @@
 const { Thought, User } = require("../models");
 
-const thoughtController = {
-  // get all Thoughts
-  getAllThought(req, res) {
-    Thought.find({})
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
-      .select("-__v")
-      .sort({ _id: -1 })
-      .then((dbThoughtData) => res.json(dbThoughtData))
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(400);
+module.exports = {
+  // Get : To get all thoughts.
+  async getAllThoughts(req, res) {
+    try {
+      const results = await Thought.find({});
+      res.status(200).json(results);
+    } catch (err) {
+      console.log(
+        'Unable to retrieve all thoughts from the database. Error: ' + err
+      );
+      res.status(500).json({
+        message:
+          'Something went wrong while retriving all thoughts from the database.',
       });
-  }, 
+    }
+  },
 
-  getThoughtById({ params }, res) {
-    Thought.findOne({ _id: params.id })
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
-      .select("-__v")
-      .then((dbThoughtData) => {
-        if (!dbThoughtData) {
-          return res.status(404).json({ message: "No thought with this id!" });
-        }
-        res.json(dbThoughtData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(400);
+  async getOneThought(req, res) {
+    try {
+      const results = await Thought.findById(req.params.id);
+      res.status(200).json(results);
+    } catch (err) {
+      console.log(
+        'Error in retrieving one thought by id from the database. Error: ' + err
+      );
+      res
+        .status(500)
+        .json({ message: 'Unable to retieve thought from the database.' });
+    }
+  },
+
+  async createThought(req, res) {
+    try {
+      const { thoughtText } = req.body;
+      if (!thoughtText) {
+        res.status(400).json({
+          message:
+            'Please ensure to enter valid text in order to update the thought.',
+        });
+      }
+      const newThought = await Thought.create({
+        thoughtText: thoughtText,
+        username: req.params.id,
       });
-  }, 
+      const user = await User.findById(req.params.id);
+      user.thoughts.push(newThought);
+      const updatedUser = await user.save();
 
-  createThought({ params, body }, res) {
-    Thought.create(body)
-      .then(({ _id }) => {
-        return User.findOneAndUpdate(
-          { _id: body.userId },
-          { $push: { thoughts: _id } },
-          { new: true }
-        );
-      })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          return res
-            .status(404)
-            .json({ message: "Thought created but no user with this id!" });
-        }
-        res.json({ message: "Thought successfully created!" });
-    })
-    .catch((err) => res.json(err));
-}}
+      res.status(200).json(newThought);
+    } catch (err) {
+      console.log(
+        'Error in creating a new thought in the database. Error: ' + err
+      );
+      res
+        .status(500)
+        .json({ message: 'Unable to create a new thought in the database.' });
+    }
+  },
+
+  async updateThought(req, res) {
+    try {
+      const updatedThought = await Thought.findByIdAndUpdate(
+        req.params.id,
+        { thoughtText: req.body.thoughtText },
+        { new: true }
+      );
+      res.status(200).json(updatedThought);
+    } catch (err) {
+      console.log(
+        'Error in updating a thought by id in the database. Error: ' + err
+      );
+      res
+        .status(500)
+        .json({ message: 'Unable to update thought in the database.' });
+      } 
+      
